dafa84577f5eb9a7fbab301834a739042de3318c
deepak <deepak@DESKTOP-DI6C2E0>
2019-04-07 18:31:35 +0530

    First Commit

---
+++ src/Addition.java
@@ -0,0 +1,14 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		b=10;
+		c=10;
+		a=b+c;
+		System.out.println(a);
+
+	}
+
+}
